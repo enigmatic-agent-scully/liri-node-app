52c1ee4a0e3419e6f42eb2810f6edfa4b00010e0
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-10-20 22:34:52 -0400

    save save save

--- liri.js
+++ liri.js
@@ -1,4 +1,15 @@
-require('dotenv').config();
+var dotenv = require('dotenv').config();
 
 var keys = import('keys.js');
+var request = require('request');
 
+var arg = process.argv;
+
+
+if (arg[2] === 'concert-this') {
+  var artist = "";
+  for (var i = 3; i > arg.length; i++) {
+    artist += `${arg[i]} `;
+  }
+
+  }
