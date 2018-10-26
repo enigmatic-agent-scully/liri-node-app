8c32e02515f9940082be01b8a934e85cc21bf699
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-10-25 23:28:29 -0400

    I DID IT

--- liri.js
+++ liri.js
@@ -4,102 +4,85 @@ var keys = require('./keys.js');
 var request = require('request');
 var moment = require('moment');
 var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
-
-
 var arg = process.argv;
+var search = arg.slice(3).join(" ");
+var spotify = new Spotify(keys.spotify)
 
+function searchSpotify(song) {
+  spotify.search({ type: 'track', query: song, limit: 10 }, function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    else {
+      console.log(data);
+      dataArr = data.tracks.items;
+      dataArr.forEach(item => {
+        console.log(`\nArtist(s): ${(item.album.artists).join(",")}\nSong: ${item.name}\nPreview: ${item.preview_url}\nAlbum: ${item.album.name}`);
+      });
+
+    }
+  })
+}
 
 if (arg[2] === 'concert-this') {
-  var artistArr = [];
-
-  for (var i = 3; i < (arg.length); i++) {
-    ;
-    artistArr.push(arg[i]);
-  }
-  var artist = artistArr.join("%20");
-  var URL = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
+  var URL = `https://rest.bandsintown.com/artists/${search}/events?app_id=codingbootcamp`;
   console.log(URL);
   request(URL, function (err, resp, body) {
-    if (!err && resp.statusCode === 200) {
-      if ((JSON.parse(body)).length < 1) {
-        console.log("This artist does not have any scheduled shows right now!");
-      }
-      else {
-        var body = JSON.parse(body);
-        console.log(body);
-        body.forEach(e => {
-          console.log("-------VENUE-------");
-          console.log(e.venue.name);
-          console.log(e.venue.city + ', ' + e.venue.region + "" + e.venue.country);
-          console.log("Date: " + moment(e.datetime).format("MM/DD/YYYY"));
-        }
-        )
-      };
+    if (!err && resp.statusCode === 200 && (JSON.parse(body)).length < 1) {
+      console.log("This artist does not have any scheduled shows right now!");
     }
-  });
+    else {
+      var body = JSON.parse(body);
+      console.log(body);
+      body.forEach(e => {
+        console.log(`\n-------VENUE-------\n${e.venue.name}\n${e.venue.city}, ${e.venue.region} ${e.venue.country} Date: ${moment(e.datetime).format("MM/DD/YYYY")}`);
+      }
+      )
+    };
+  }
+  );
 }
 else if (arg[2] === 'spotify-this-song') {
-  var songArr = [];
-  var spotify = new Spotify(keys.spotify);
-  for (var i = 3; i < arg.length; i++) {
-    songArr.push(arg[i]);
-  }
-  var song = songArr.join(" ");
-
-spotify.search({type: 'track', query: song}, function(err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data.tracks.items);
-})
+  console.log(search);
+  searchSpotify(search);
 }
 else if (arg[2] === 'movie-this') {
-  if (arg[3] != null) {
-    var movieArr = [];
-
-    for (var i = 3; i < arg.length; i++) {
-      movieArr.push(arg[i]);
+  var omdbURL = `http://www.omdbapi.com/?apikey=trilogy&t=${search}`
+  request(omdbURL, function (err, resp, body) {
+    if (!err && resp.statusCode === 200) {
+      m = JSON.parse(body);
+      console.log(`\nTitle: ${m.Title}\nYear Released: ${m.Year}\nIMDB Raiting: ${m.Ratings[0].Value}\nRotten Tomatoes Rating: ${m.Ratings[1].Value}\nProduced in: ${m.Country}\nLanguage: ${m.Language}\nPlot: ${m.Plot}\nCast: ${m.Actors}`);
     }
-    var movie = movieArr.join("%20");
-    var omdbURL = `http://www.omdbapi.com/?apikey=trilogy&t=${movie}`
-    console.log(omdbURL);
-    request(omdbURL, function (err, resp, body) {
-      if (!err && resp.statusCode === 200) {
-        m = JSON.parse(body);
-        console.log(`Title: ${m.Title}`);
-        console.log(`Year Released: ${m.Year}`);
-        console.log(`IMDB Raiting: ${m.Ratings[0].Value}`);
-        console.log(`Rotten Tomatoes Rating: ${m.Ratings[1].Value}`);
-        console.log(`Produced in: ${m.Country}`);
-        console.log(`Language: ${m.Language}`);
-        console.log(`Plot: ${m.Plot}`);
-        console.log(`Cast: ${m.Actors}`);
+    else {
+      var omdbURL = `http://www.omdbapi.com/?apikey=f0db77fd&t=Mr%20Nobody`
+      request(omdbURL, function (err, resp, body) {
+        if (!err && resp.statusCode === 200) {
+          m = JSON.parse(body);
+          console.log(`\nTitle: ${m.Title}\nYear Released: ${m.Year}\nIMDB Raiting: ${m.Ratings[0].Value}\nRotten Tomatoes Rating: ${m.Ratings[1].Value}\nProduced in: ${m.Country}\nLanguage: ${m.Language}\nPlot: ${m.Plot}\nCast: ${m.Actors}`);
+        }
       }
-    })
+    )
+    }
   }
-  else {
-    var movie = "Mr%20Nobody";
-    var omdbURL = `http://www.omdbapi.com/?apikey=f0db77fd&t=${movie}`
-    console.log(omdbURL);
-    request(omdbURL, function (err, resp, body) {
-      if (!err && resp.statusCode === 200) {
-        m = JSON.parse(body);
-        console.log(`Title: ${m.Title}`);
-        console.log(`Year Released: ${m.Year}`);
-        console.log(`IMDB Raiting: ${m.Ratings[0].Value}`);
-        console.log(`Rotten Tomatoes Rating: ${m.Ratings[1].Value}`);
-        console.log(`Produced in: ${m.Country}`);
-        console.log(`Language: ${m.Language}`);
-        console.log(`Plot: ${m.Plot}`);
-        console.log(`Cast: ${m.Actors}`);
-      }
-    }
-    )}
+  )
 }
+
 else if (arg[2] === 'do-what-it-says') {
   var fs = require('fs');
-  // fs.INSERT_CORRECT_METHOD('random.txt', 'utf8', err){
-    
-  // }
+  fs.readFile('random.txt', 'utf8', (err, text) => {
+    if (!err) {
+      var textArr = text.split(',');
+      console.log(textArr);
+      if (textArr[0] === 'spotify-this-song') {
+        var s = textArr[1].substr(1, (textArr[1].length - 2));
+        search = s.split(" ").join(" ");
+        searchSpotify(search);
+      }
+    }
+    else {
+      (console.log(err));
+    }
+
+  }
+  )
 }
